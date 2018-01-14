b4dda194ff40711987a9842e749ef5e89f6e3629
Brandon Olin <brandon@devblackops.io>
2018-01-13 21:00:23 -0800

    Add solution to puzzle 1

---
+++ Prequel1/Get-MonitorDetail.ps1
@@ -0,0 +1,45 @@
+function Get-MonitorDetail {
+    [OutputType('PSCustomObject')]
+    [cmdletbinding()]
+    param(
+        [parameter(ValueFromPipeline, ValueFromPipelineByPropertyName = $true)]
+        [Alias('Name')]
+        [string[]]$ComputerName = (hostname) # Support macOS/Linux/Windows
+    )
+
+    begin {
+        $myComputerName = hostname
+        $PSDefaultParameterValues = @{
+            'Get-CimInstance:Verbose' = $false
+        }
+    }
+
+    process {
+        foreach ($computer in $ComputerName) {
+            # Support remote machines
+            $cimParams = @{}
+            if ($computer -ne $myComputerName) {
+                $cimParams.ComputerName = $computer
+            }
+
+            Write-Verbose -Message "Quering computer [$computer]"
+            try {
+                $computerInfo = Get-CimInstance -ClassName Win32_ComputerSystem @cimParams -ErrorAction Stop
+                $serialNumber = Get-CimInstance -ClassName Win32_Bios @cimParams | Select-Object -ExpandProperty SerialNumber
+                $monitors = Get-CimInstance -ClassName wmiMonitorID -Namespace root\wmi @cimParams
+                $monitors | ForEach-Object {
+                    [PSCustomObject]@{
+                        PSTypeName     = 'MonitorDetail'
+                        ComputerName   = $computer
+                        ComputerType   = $computerInfo.Model
+                        ComputerSerial = $serialNumber
+                        MonitorSerial  = [System.Text.Encoding]::Default.GetString($_.SerialNumberID)
+                        MonitorType    = [System.Text.Encoding]::Default.GetString($_.UserFriendlyName)
+                    }
+                }
+            } catch {
+                Write-Error -Message "Unable to connect to [$computer]"
+            }
+        }
+    }
+}

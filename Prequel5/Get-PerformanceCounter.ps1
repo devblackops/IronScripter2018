7837ed99704b5f89c02af8791a130265b3493f17
Brandon Olin <brandon@devblackops.io>
2018-02-13 22:20:18 -0800

    Add support for credentials and cim sessions

--- Prequel5/Get-PerformanceCounter.ps1
+++ Prequel5/Get-PerformanceCounter.ps1
@@ -1,43 +1,78 @@
 
 function Get-PerformanceCounter {
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName = 'Computer')]
     param(
-        [parameter(ValueFromPipeline)]
+        [parameter(
+            ParameterSetName = 'Computer',
+            ValueFromPipeline = $true
+        )]
         [ValidateNotNullOrEmpty()]
         [Alias('Name')]
-        [string[]]$ComputerName = $env:COMPUTERNAME
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+
+        [parameter(
+            ParameterSetName = 'Computer'
+        )]
+        [System.Management.Automation.CredentialAttribute()]
+        [pscredential]$Credential,
+
+        [parameter(
+            ParameterSetName = 'CimSession',
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Microsoft.Management.Infrastructure.CimSession[]]$CimSession
     )
 
     process {
         foreach ($Computer in $ComputerName) {
-            Write-Verbose -Message "Gathering performance counters for [$ComputerName]"
+            try {
+                Write-Verbose -Message "Gathering performance counters for [$ComputerName]"
 
-            # Gather disk, memory, processor, and network stats
-            $counters = @(
-                '\\{0}\LogicalDisk(c:)\% Free Space'       -f $Computer
-                '\\{0}\Memory\% Committed Bytes In Use'    -f $Computer
-                '\\{0}\Processor(_Total)\% Processor Time' -f $Computer
-            )
-            $disk = Get-Counter -Counter $counters[0]
-            $mem  = Get-Counter -Counter $counters[1]
-            $proc = Get-Counter -Counter $counters[2]
-            $netCounters = Get-NetAdapter -Physical | ForEach-Object {
-                $label = $_.InterfaceDescription.Replace('#', '_')
-                "\Network Adapter($label)\Bytes Total/sec"
-            }
-            $net  = Get-Counter -Counter $netCounters
-            $totalNetBytes = ($net.CounterSamples |
-                Sort-Object -Property CookedValue -Descending |
-                Select-Object -First 2 |
-                Measure-Object -Property CookedValue -Sum).Sum
+                # Support remote machines with optional credential
+                $cimParams = @{}
+                if ($PSCmdlet.ParameterSetName -eq 'Computer') {
+                    if ($Computer -ne $env:COMPUTERNAME) {
+                        if ($Credential) {
+                            $cimParams.CimSession = New-CimSession -ComputerName $Computer -Credential $Credential
+                        } else {
+                            $cimParams.ComputerName = $Computer
+                        }
+                    }
+                } else {
+                    $cimParams.CimSession = $CimSession
+                }
 
-            [pscustomobject]@{
-                ComputerName       = $Computer
-                Timestamp          = Get-Date
-                PctProcessorTime   = "{0:n2}" -f $proc.CounterSamples.CookedValue
-                PctFreeSpaceC      = "{0:n2}" -f $disk.CounterSamples.CookedValue
-                PctCommittedBytes  = "{0:n2}" -f $mem.CounterSamples.CookedValue
-                NetworkBytesPerSec = "{0:n2}" -f $totalNetBytes
+                # Gather disk, memory, processor, and network stats
+                $disk = Get-Counter -Counter ('\\{0}\LogicalDisk(c:)\% Free Space'       -f $Computer)
+                $mem  = Get-Counter -Counter ('\\{0}\Memory\% Committed Bytes In Use'    -f $Computer)
+                $proc = Get-Counter -Counter ('\\{0}\Processor(_Total)\% Processor Time' -f $Computer)
+                $netCounters = Get-NetAdapter -Physical @cimParams | ForEach-Object {
+                    $label = $_.InterfaceDescription.Replace('#', '_')
+                    "\Network Adapter($label)\Bytes Total/sec"
+                }
+                $net = Get-Counter -Counter $netCounters
+                $totalNetBytes = ($net.CounterSamples |
+                    Sort-Object -Property CookedValue -Descending |
+                    Select-Object -First 2 |
+                    Measure-Object -Property CookedValue -Sum).Sum
+
+                [pscustomobject]@{
+                    ComputerName       = $Computer
+                    Timestamp          = Get-Date
+                    PctProcessorTime   = "{0:n2}" -f $proc.CounterSamples.CookedValue
+                    PctFreeSpaceC      = "{0:n2}" -f $disk.CounterSamples.CookedValue
+                    PctCommittedBytes  = "{0:n2}" -f $mem.CounterSamples.CookedValue
+                    NetworkBytesPerSec = "{0:n2}" -f $totalNetBytes
+                }
+            } catch {
+                Write-Error $_
+            } finally {
+                # Don't remove the existing CIM session that was passed in
+                # It still may be needed upstream
+                if ($PSCmdlet.ParameterSetName -eq 'Computer' -and $cimParams.CimSession) {
+                    $cimParams.CimSession | Remove-CimSession
+                }
             }
         }
     }
